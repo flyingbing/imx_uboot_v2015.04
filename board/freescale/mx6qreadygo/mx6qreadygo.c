0e32eb680b5516765cbfd5739eab20aaed16b5e5
= <=>
2016-06-08 16:08:24 +0800

    first commit

--- board/freescale/mx6qreadygo/mx6qreadygo.c
+++ board/freescale/mx6qreadygo/mx6qreadygo.c
@@ -354,7 +354,8 @@ int mmc_map_to_kernel_blk(int dev_no)
 	return dev_no + 1;
 }
 
-#define USDHC2_CD_GPIO	IMX_GPIO_NR(2, 2)
+//#define USDHC2_CD_GPIO	IMX_GPIO_NR(2, 2)
+#define USDHC2_CD_GPIO	IMX_GPIO_NR(1, 4)
 #define USDHC3_CD_GPIO	IMX_GPIO_NR(2, 0)
 
 int board_mmc_getcd(struct mmc *mmc)
